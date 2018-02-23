202fe126ae36ae19d4dc55a819a84e3dd0fde5b6
Edin Jašarević <edinjasar14@gmail.com>
2018-02-23 14:46:46 +0100

    pr1 vjezbe zadatak15

---
+++ pr1/vjezbe/Zadatak15/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Napisite  program,  postujuci  sve  faze  procesa  programiranja,    koji  ce  za 
+	uneseno  malo  slovo  engleske  abecede  ispisati  to  isto  slovo  samo  veliko  i  obrnuto,  za 
+	uneseno veliko slovo ispisati malo. Obavezno upotrijebite uvjetni operator. 
+*/
+
+int main()
+{
+	char slovo;
+
+	cout << "Unesite slovo: ";
+	cin >> slovo;
+
+	((slovo >= 'a') && (slovo <= 'z')) ? cout << char(slovo - 32) : cout << char(slovo + 32) << endl;
+
+	system("pause");
+	return 0;
+}
