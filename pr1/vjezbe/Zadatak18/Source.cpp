0fd0aae5f1f7f15bdeb681e5b21bc11887930b02
Edin Jašarević <edinjasar14@gmail.com>
2018-02-23 15:39:03 +0100

    pr1 vjezbe zadatak18

---
+++ pr1/vjezbe/Zadatak18/Source.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Napraviti  program  koji  ce  na  osnovu  dva  unesena  realna  broja  i  znaka  za 
+	operaciju  (+,  -,  *  i  /)  izvrsiti  operaciju  nad  unesenim  brojevima.  
+	Postujte  sve  faze procesa programiranja. 
+*/
+
+int main()
+{
+	char operacija;
+	float b1, b2, rez = 0.0F;
+
+	cout << "Unesite dva broja:\n";
+	cin >> b1 >> b2;
+	cout << "Unesite operaciju: ";
+	cin >> operacija;
+
+	cout << "Rezultat je: ";
+	
+	switch (operacija)
+	{
+	case '+':
+		rez = b1 + b2;
+		cout << rez << endl;
+		break;
+	case '-':
+		rez = b1 - b2;
+		cout << rez << endl;
+		break;
+	case '*':
+		rez = b1 * b2;
+		cout << rez << endl;
+		break;
+	case '/':
+		rez = b1 / b2;
+		cout << rez << endl;
+		break;
+	default:
+		cout << "Pogresna operacija!\n";
+		break;
+	}
+
+	system("pause");
+	return 0;
+}
