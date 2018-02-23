9cd7a21fcc89b1804b5267ea1e666cef3bd27e75
Edin Jašarević <edinjasar14@gmail.com>
2018-02-23 19:09:51 +0100

    pr vjezbe zadatak23

---
+++ pr1/vjezbe/Zadatak23/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Napisite program, postujuci sve faze procesa programiranja, za ispis prirodnih brojeva 
+	u intervalu od 1 do 100. Nakon svakog petog (5) broja potrebno je ispisati novi red. 
+*/
+
+int main()
+{
+	int broj = 1;
+
+	while (broj <= 100)
+	{
+		cout << broj;
+		if (broj % 5 == 0)
+			cout << endl;
+
+		broj++;
+	}
+
+	system("pause");
+	return 0;
+}
