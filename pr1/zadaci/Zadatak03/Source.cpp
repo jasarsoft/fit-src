a5043b0410b0874ed2bef34c8b7b9d8f8794ac3c
Edin Jašarević <edinjasar14@gmail.com>
2018-02-25 15:01:37 +0100

    pr1 zadaci zadatak03

---
+++ pr1/zadaci/Zadatak03/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //#include <iostream.h>
+
+/*
+	Sta morate izmijeniti da bi programi iz zadatka br. 2 radili i 
+	u starom kompajleru Borland C++? Prepravite kod!
+*/
+
+void main()
+{
+	std::cout << "Hello World" << std::endl; //cout << "Hello World\n";
+}
