50aa6a89050fb386b0b77f4f947113d178b07e4a
Edin Jašarević <edinjasar14@gmail.com>
2018-02-25 15:43:05 +0100

    pr1 zadaci zad09

---
+++ pr1/zadaci/Zadatak09/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Prepravite prethodni program iz zadatka 6: 
+	a) tako da za ispis prve poruke koja se nalazi u dva reda koristite samo jedan cout 
+	b) tako da za ispis druge poruke koja se nalazi u dva reda koristite samo jedan cout 
+*/
+
+int main()
+{
+	float pi = 3.14F;
+
+	cout << "Vrijednost za pi je: " << pi << endl;
+
+	cout << "Vrijednost kvadrata od pi je: " << pi * pi << endl;
+
+	system("pause");
+	return 0;
+}
