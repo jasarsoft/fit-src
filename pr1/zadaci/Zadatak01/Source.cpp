ff364778993334da9ba95111e2a0f7b67ce6e55d
Edin Jašarević <edinjasar14@gmail.com>
2018-02-25 14:55:25 +0100

    pr1 zadaci zadatak01

---
+++ pr1/zadaci/Zadatak01/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Napravite program 'Hello World' u Visual C++-u (2003/2005/2008): 
+	a)  tako sto cete koristiti void main() 
+	b)  tako sto cete koristiti int main() 
+*/
+
+int main()
+{
+	cout << "Hello World\n";
+	
+	system("pause");
+	return 0;
+}
