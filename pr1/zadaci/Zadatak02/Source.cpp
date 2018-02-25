4b1c8e3c33604e11273ccaa3a7d2073d641ccc8c
Edin Jašarević <edinjasar14@gmail.com>
2018-02-25 14:57:28 +0100

    pr1 zadaci zadatak02

---
+++ pr1/zadaci/Zadatak02/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*
+	Napravite program 'Hello World' bez koristenja system("PAUSE") 
+	a)  tako sto cete koristiti void main() 
+	b)  tako sto æete koristiti int main()
+*/
+
+void main()
+{
+	std::cout << "Hello World" << std::endl;
+
+}
