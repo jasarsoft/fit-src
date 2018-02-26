e42ceee7b728c05c3eb8be9d06f6e8cbe7ecbfe3
Edin Jašarević <edinjasar14@gmail.com>
2018-02-26 20:16:41 +0100

    pr1 zadaci zad24

---
+++ pr1/zadaci/Zadatak24/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Deklarisite varijable a1 i a2 tipa char. Varijabli a1 dodijelite znak '@', a varijabli a2 
+	dodijelite broj 64.
+*/
+
+int main()
+{
+	char a1, a2;
+
+	a1 = '@';
+	a2 = 64;
+
+	cout << "Varijabla a1: " << a1 << endl;
+	cout << "Varijabla a2: " << a2 << endl;
+
+	system("pause");
+	return 0;
+}
