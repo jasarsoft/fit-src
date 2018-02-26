afbc8cca209180de56ef133bb750be6e5f5ae422
Edin Jašarević <edinjasar14@gmail.com>
2018-02-26 20:20:06 +0100

    pr1 zadaci zad25

---
+++ pr1/zadaci/Zadatak25/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Da li brojevi imaju svoj ASCII kod? Saznajte to na isti nacin kao sto se saznali koji su 
+	ASCII kodovi za velika i mala slova engleske abecede! 
+*/
+
+int main()
+{
+	char b1, b2;
+
+	b1 = '1';
+	b2 = 1;
+
+	cout << "Vrijednost varijable b1: " << b1 << endl;
+	cout << "Vrijednost varijable b2: " << b2 << endl;
+
+	system("pause");
+	return 0;
+}
