d777e70b002289bf0982b8196902fbe480726dc9
Edin Jašarević <edinjasar14@gmail.com>
2018-02-26 20:27:58 +0100

    pr1 zadaci zad27

---
+++ pr1/zadaci/Zadatak27/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Da li je sljedeci program ispravan? Ako nije, ispravite greske! 
+
+	void main()
+	{
+		cout << "@ + # = "<< char('@' + '#') << endl;
+		cout << "Ovaj program zna raèunati!" << char(2) << endl;
+	}
+*/
+
+int main()
+{
+	cout << "@ + # = " << char('@' + '#') << endl;
+	cout << "Ovaj program zna racunati! " << char(2) << endl;
+
+	system("pause");
+	return 0;
+}
