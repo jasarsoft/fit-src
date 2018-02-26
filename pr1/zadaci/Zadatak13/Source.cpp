cdc23bd3de101ee808086c76278d98cb6a3a0e4d
Edin Jašarević <edinjasar14@gmail.com>
2018-02-26 15:20:55 +0100

    pr1 zadaci zad13

---
+++ pr1/zadaci/Zadatak13/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Napravite program koji ce ispisati sljedeci tekst, koristeci “\t“ za tabulator 
+	Ime:			Ismet 
+	Prezime:		Maksumic 
+	Radno mjesto:	FIT 
+*/
+
+int main()
+{
+	cout << "Ime:\t\tIsmet" << endl;
+	cout << "Prezime:\tMaksumic" << endl;
+	cout << "Radno mjesto:\tFIT" << endl;
+
+	system("pause");
+	return 0;
+}
