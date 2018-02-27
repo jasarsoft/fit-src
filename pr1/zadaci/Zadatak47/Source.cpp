f4d1243adc523959845e8a07f4d80d57b482e5ed
Edin Jašarević <edinjasar14@gmail.com>
2018-02-27 22:29:15 +0100

    pr1 zadaci zad47

---
+++ pr1/zadaci/Zadatak47/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Isprobajte sljedeci program. On vam daje odgovor na pitanje koju vrijednost imaju sta 
+	je uslov i cemu je on jednak? 
+*/
+
+int main()
+{
+	int a = 5;
+	int b = 0;
+
+	cout << "Zadovoljen uslov je isto kao " << (a>b) << endl;
+
+	cout << "Nezadovoljen uslov je isto kao " << (a<b) << endl;
+
+	system("pause");
+	return 0;
+}
