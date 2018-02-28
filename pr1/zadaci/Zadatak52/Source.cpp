44dc6c7f0536da8e574439695b8b1458469b4a46
Edin Jašarević <edinjasar14@gmail.com>
2018-02-28 10:38:41 +0100

    pr1 zadaci zad52

---
+++ pr1/zadaci/Zadatak52/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Napravite program koji ima samo jednu liniju koda i to cout-naredbu za ispis vrijednosti:  
+	
+	20 / 2 * 5
+	
+*/
+
+int main()
+{
+	cout << (20 / (2 * 5)) << endl;
+
+	system("pause");
+	return 0;
+}
