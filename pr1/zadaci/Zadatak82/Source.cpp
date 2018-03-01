94be9167d739b2e9f6764b82acd5de9243ab7701
Edin Jašarević <edinjasar14@gmail.com>
2018-03-01 18:20:56 +0100

    pr1 zadaci zad82

---
+++ pr1/zadaci/Zadatak82/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Prepravite prethodni program tako da koristite for-petlju.
+*/
+
+int main()
+{
+	int y = 0;
+
+	for (int i = 1; i <= 6; i++)
+		y += 7;
+
+	cout << "Vrijednost y: " << y << endl;
+
+	system("pause");
+	return 0;
+}
