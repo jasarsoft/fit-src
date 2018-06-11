46f04c8faf1a89d0ea0ecff8b4183f46a6c143cc
Edin Jašarević <edinjasar14@gmail.com>
2018-06-12 01:19:56 +0200

    bp2 predavanje | union

---
+++ bp2/predavanja/51.union.sql
@@ -0,0 +1,9 @@
+USE AdventureWorks2014
+GO
+SELECT	ProductModelID AS [Product ID],
+		ModifiedDate AS [Date of modification]
+FROM Production.ProductModelProductDescriptionCulture
+UNION
+SELECT	ProductModelID AS [Product ID],
+		ModifiedDate AS [Date of modification]
+FROM Production.ProductModel
