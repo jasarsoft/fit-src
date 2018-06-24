f779526f4a84b6fa93ada6abbfa8c68253ebd8a2
Edin Jašarević <edinjasar14@gmail.com>
2018-06-24 18:36:06 +0200

    bp2 pred | kreiranje procedure

---
+++ bp2/predavanja/pred19_q05_create_procedure.sql
@@ -0,0 +1,19 @@
+--kreiranje procedure
+
+USE TestDB
+GO
+
+CREATE PROCEDURE Sales.GetSalesPersonNames
+AS
+BEGIN
+	SELECT SP.BusinessEntityID, P.LastName, P.FirstName
+	FROM Sales.SalesPerson AS SP
+	INNER JOIN Person.Person AS P
+	ON SP.BusinessEntityID = P.BusinessEntityID
+	WHERE SP.TerritoryID IS NOT NULL
+	ORDER BY SP.BusinessEntityID
+END;
+
+--izvrsenje procedure
+EXECUTE Sales.GetSalesPersonNames 
+GO
