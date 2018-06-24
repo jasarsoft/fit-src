b7c6b2aaaa750048ce88059bc54a913a5b8e9d1a
Edin Jašarević <edinjasar14@gmail.com>
2018-06-24 18:37:52 +0200

    bp2 pred19 | izlazni parametar procedure

---
+++ bp2/predavanja/pred19_q10_output_procedure.sql
@@ -0,0 +1,20 @@
+--procedura sa izlaznim parametrima
+
+USE TestDB
+GO
+
+CREATE PROC Sales.GetOrderCountByDueDate
+	@DueDate datetime, @OrderCount int OUTPUT
+AS
+	SELECT @OrderCount = COUNT(1)
+	FROM Sales.SalesOrderHeader
+	WHERE DueDate = @DueDate;
+GO
+
+DECLARE @DueDate datetime = '20140712';
+DECLARE @OrderCount int;
+
+EXEC Sales.GetOrderCountByDueDate @DueDate,
+								  @OrderCount OUTPUT;
+
+SELECT @OrderCount;
