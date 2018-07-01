244b9c3abbd3a8303eb9a940a07c2c3da286c5ab
Edin Jašarević <edinjasar14@gmail.com>
2018-07-01 17:16:55 +0200

    bp2 pred27 | kreiranje certifikata

---
+++ bp2/predavanja/pred27_q03_certificate.sql
@@ -0,0 +1,7 @@
+--create a certificate
+
+CREATE CERTIFICATE Cert4SymKey
+ENCRYPTION BY PASSWORD = 'pGFD4bb925DGvbd2439587y'
+WITH SUBJECT = 'Guardian of Symmetric key',
+EXPIRY_DATE = '20201031';
+GO
