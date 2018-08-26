d9b963be3afbd4494de9d0289c947b0a41ad91d7
Edin Jašarević <edinjasar14@gmail.com>
2018-08-26 21:11:17 +0200

    bp2 vj10pdf | dodjela ovlasti za pogled

---
+++ bp2/vjezbe_pdf/bp2_vj10pdf_zadatak08.sql
@@ -0,0 +1,10 @@
+/*
+	Vjezbe 10 Zadatak 8
+
+	Kreiranom korisniku dozvoliti citanje podataka o osobama iskljucivo putem view-a. 
+	Testirati dodijeljene permisije! 
+*/
+
+USE Edin
+
+GRANT SELECT ON OBJECT::Osobe.vOsobe TO uEdin
